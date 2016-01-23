753130451a8fffe54ae59f53844a2dddec1494a3
rmc2 <brindaannthomas@gmail.com>
2016-01-23 14:50:33 -0800

    adding hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"github.com/rmc2/golang_example/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    //fmt.Printf("hello, world\n")
 }
